7cd67f5dcb449b472a3515d28b07d5016587b041
akozubski@comperia.pl <akozubski@comperia.pl>
2021-11-08 17:30:47 +0100

    FN-8165 Update offers processing.

--- includes/wc-comfino-gateway.php
+++ includes/wc-comfino-gateway.php
@@ -166,7 +166,7 @@ class WC_Comfino_Gateway extends WC_Payment_Gateway
             $paymentInfos[] = [
                 'name' => $offer['name'],
                 'description' => $offer['description'],
-                'icon' => str_replace('<?xml version="1.0" encoding="UTF-8"?>', '', $offer['icon']),
+                'icon' => str_ireplace('<?xml version="1.0" encoding="UTF-8"?>', '', $offer['icon']),
                 'type' => $offer['type'],
                 'sumAmount' => number_format($total / 100, 2, ',', ' '),
                 'representativeExample' => $offer['representativeExample'],
