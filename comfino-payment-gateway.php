f17824171a244c997d8b3101baa32f964fb07d71
Marcin Chyłek <mchylek@comperia.pl>
2022-05-18 12:29:01 +0200

    Tested WP 5.9.3 or 5.4.2

--- comfino-payment-gateway.php
+++ comfino-payment-gateway.php
@@ -3,7 +3,7 @@
  * Plugin Name: Comfino Payment Gateway
  * Plugin URI: https://github.com/comfino/WooCommerce.git
  * Description: Comfino (Comperia) - Comfino Payment Gateway for WooCommerce.
- * Version: 2.2.2
+ * Version: 2.2.3
  * Author: Comfino (Comperia)
  * Author URI: https://github.com/comfino
  * Domain Path: /languages
