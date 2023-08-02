bccad1f7c4b6f21905ae49c50f217bf9e362b405
akozubski@comperia.pl <akozubski@comperia.pl>
2023-08-02 16:25:32 +0200

    FN-10721 Fix bug in payment process - PAY_LATER didn't work properly.

--- includes/assets/js/comfino.js
+++ includes/assets/js/comfino.js
@@ -72,7 +72,7 @@ window.Comfino = {
                 }
             }
         } else {
-            document.getElementById('comfino-loan-term').value = '1';
+            document.getElementById('comfino-loan-term').value = '';
             document.getElementById('comfino-total-payment').innerHTML = Comfino.offerList.data[Comfino.selectedOffer].sumAmount + ' zł';
         }
     },
@@ -103,7 +103,7 @@ window.Comfino = {
                 }
             }
         } else {
-            document.getElementById('comfino-loan-term').value = '';
+            document.getElementById('comfino-loan-term').value = '1';
         }
     },
 
