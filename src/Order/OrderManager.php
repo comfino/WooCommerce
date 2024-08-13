2291b28738ab207c1d094b719f261f144570e727
Artur Kozubski <akozubski@comperia.pl>
2024-08-13 16:10:16 +0200

    FN-11337 Plugin refactoring. OrderManager updated.

--- src/Order/OrderManager.php
+++ src/Order/OrderManager.php
@@ -6,7 +6,6 @@ use Comfino\Common\Shop\Cart;
 use Comfino\Shop\Order\Cart\CartItem;
 use Comfino\Shop\Order\Cart\CartItemInterface;
 use Comfino\Shop\Order\Cart\Product;
-use Comfino\Tools;
 
 if (!defined('ABSPATH')) {
     exit;
@@ -53,21 +52,21 @@ final class OrderManager
         );
     }
 
-    public static function getShopCartFromProduct(\Product $product): Cart
+    public static function getShopCartFromProduct(\WC_Product_Simple $product): Cart
     {
         return new Cart(
-            (int) ($product->getPrice() * 100),
+            (int) (wc_get_price_including_tax($product) * 100),
             0,
             [
                 new CartItem(
                     new Product(
-                        is_array($product->name) ? current($product->name) : $product->name,
-                        (int) ($product->getPrice() * 100),
-                        (string) $product->id,
+                        $product->get_name(),
+                        (int) (wc_get_price_including_tax($product) * 100),
+                        (string) $product->get_id(),
                         null,
                         null,
                         null,
-                        array_map('intval', $product->getCategories())
+                        $product->get_category_ids()
                     ),
                     1
                 ),
@@ -75,33 +74,19 @@ final class OrderManager
         );
     }
 
-    public static function checkCartCurrency(\PaymentModule $module, \Cart $cart): bool
+    public static function getOrderStatusNotes(int $orderId, array $statuses): array
     {
-        $currencyOrder = new \Currency($cart->id_currency);
-        $currenciesModule = $module->getCurrency($cart->id_currency);
+        $orderNotes = wc_get_order_notes(['order_id' => $orderId]);
+        $notes = [];
 
-        if (is_array($currenciesModule)) {
-            foreach ($currenciesModule as $currencyModule) {
-                if ((int) $currencyOrder->id === (int) $currencyModule['id_currency']) {
-                    return true;
+        foreach ($orderNotes as $note) {
+            foreach ($statuses as $status) {
+                if ($note->added_by === 'system' && $note->content === "Comfino status: $status") {
+                    $notes[$status] = $note;
                 }
             }
         }
 
-        return false;
-    }
-
-    public static function validateCustomerData(\PaymentModule $module, array $params): string
-    {
-        $vatNumber = $params['form']->getField('vat_number');
-        $tools = new Tools(\Context::getContext());
-
-        if (!empty($vatNumber->getValue()) && !$tools->isValidTaxId($vatNumber->getValue())) {
-            $vatNumber->addError($module->l('Invalid VAT number.'));
-
-            return '0';
-        }
-
-        return '1';
+        return $notes;
     }
 }
