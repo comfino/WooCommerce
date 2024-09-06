0b2c4277c6143ef994312d18de248857b5dceda8
Artur Kozubski <akozubski@comperia.pl>
2024-09-06 17:24:37 +0200

    FN-11337 Plugin refactoring. Updated paywall block script.

--- resources/js/front/paywall.js
+++ resources/js/front/paywall.js
@@ -1,35 +1,29 @@
 const settings = window.wc.wcSettings.getSetting('comfino_data', {});
-const label = window.wp.htmlEntities.decodeEntities(settings.title) || window.wp.i18n.__('Comfino Payments', 'comfino-payment-gateway');
+const label = window.wp.htmlEntities.decodeEntities(settings.title) || window.wp.i18n.__('Comfino payments', 'comfino-payment-gateway');
 
 const Label = () => {
     if (settings.icon) {
-        return window.wp.element.createElement(
-            'img',
-            {
-                src: settings.icon,
-                alt: settings.title,
-                style: { float: 'right', marginRight: '20px' }
-            }
-        );
+        return window.wp.element.RawHTML({
+            children: label + '<img src="' + settings.icon + '" alt="' + label + '" style="margin-left: 10px; vertical-align: bottom">'
+        });
     }
 
     return label;
 };
 
 const Content = () => {
-    return window.wp.htmlEntities.decodeEntities(settings.description || 'TEST');
+    return window.wp.htmlEntities.decodeEntities(settings.description || '');
 };
 
-const Comfino = {
+window.wc.wcBlocksRegistry.registerPaymentMethod({
     name: 'comfino',
     label: Object(window.wp.element.createElement)(Label, null),
+    icon: 'money-alt',
     content: Object(window.wp.element.createElement)(Content, null),
-    edit: Object(window.wp.element.createElement)(Content, null),
+    edit: Object(window.wp.element.createElement)('EDIT', null),
     canMakePayment: () => true,
     ariaLabel: label,
     supports: {
         features: settings.supports
     }
-};
-
-window.wc.wcBlocksRegistry.registerPaymentMethod(Comfino);
+});
