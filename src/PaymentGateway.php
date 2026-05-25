ece5aae13a09ded06a48b3937528eddbbf00df25
Artur Kozubski <akozubski@comperia.pl>
2026-05-25 15:56:54 +0200

    FN-14460 Updated `BUILD_TS` and modified `composer.json` to remove author and adjust repository path configuration.

--- src/PaymentGateway.php
+++ src/PaymentGateway.php
@@ -32,7 +32,7 @@ class PaymentGateway extends \WC_Payment_Gateway
 {
     public const GATEWAY_ID = 'comfino';
     public const VERSION = '4.3.0';
-    public const BUILD_TS = 1779456607;
+    public const BUILD_TS = 1779716276;
     public const WIDGET_INIT_SCRIPT_HASH = '0603f4e0904fd65e2aef1aded0c57c40';
     public const WIDGET_INIT_SCRIPT_LAST_HASH = '55e4306bb493ff6f99b2f8f617e18038';
 
